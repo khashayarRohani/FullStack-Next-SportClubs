bfad59e83a02938d9b9ee34f8995b2e5a0395d00
khashayarRohani <khashayarrhi@gmail.com>
2024-08-26 17:07:29 +0100

    checking by removing .next and mpdifying config

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
       },
     ],
   },
+  webpack(config, { dev }) {
+    if (dev) {
+      config.devtool = "cheap-module-source-map"; // Adjust as needed
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
