daa58f193686fbd54b2ae0d79782a6df7871dcff
khashayarRohani <khashayarrhi@gmail.com>
2024-08-26 17:15:48 +0100

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -8,12 +8,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config, { dev }) {
-    if (dev) {
-      config.devtool = "cheap-module-source-map"; // Adjust as needed
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
