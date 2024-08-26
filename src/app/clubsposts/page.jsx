38468b523ff32a8f6b2d32cafa593b31761aa22a
khashayarRohani <khashayarrhi@gmail.com>
2024-08-26 16:19:10 +0100

    checking rout

--- src/app/clubsposts/page.jsx
+++ src/app/clubsposts/page.jsx
@@ -2,7 +2,7 @@ import { db } from "@/lib/db";
 import Link from "next/link";
 import Image from "next/image";
 
-import "./ppst.css";
+import "@/app/clubsposts/ppst.css";
 import { headers } from "next/headers";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
